b9742564a2c5ea61228f853d35515b2faeeeea7d
Charles Lewis <charklewis@users.noreply.github.com>
2023-09-30 15:37:50 +1000

    feat: add initial project template files (#1)

---
+++ app/root.tsx
@@ -0,0 +1,35 @@
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css";
+
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+const links: LinksFunction = () => [{ rel: "stylesheet", href: stylesheet }];
+
+function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export { links };
+export default App;
