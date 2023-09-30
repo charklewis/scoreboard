129921e3117014b2b3233bebdbe7194cffe92b09
Charles Lewis <charles_lewis@icloud.com>
2023-10-01 09:33:26 +1100

    feat: update to cross-fetch

--- app/database/db.ts
+++ app/database/db.ts
@@ -2,11 +2,13 @@ import { connect } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import * as schema from "./schema";
 
-import fetchPolyfill from "fetch-ponyfill";
+//note this is needed for vercel, otherwise all queries error with
+//First parameter has member 'readable' that is not a ReadableStream
+import fetch from "cross-fetch";
 
 const connection = connect({
   url: process.env["DATABASE_URL"] as string,
-  fetch: fetchPolyfill().fetch,
+  fetch,
 });
 
 const db = drizzle(connection, { schema });
