b842cf7edde45d051b722c64095199dc785ae297
Charles Lewis <charles_lewis@icloud.com>
2023-09-30 22:42:15 +1000

    ci: try pollyfill

--- app/database/db.ts
+++ app/database/db.ts
@@ -2,6 +2,8 @@ import { connect } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import * as schema from "./schema";
 
+import fetchPonyfill from "fetch-ponyfill";
+
 const connection = connect({
   url: process.env["DATABASE_URL"] as string,
 });
