6dddf25e8c7add4ab4e3f4741a0acca512cb0a61
Charles Lewis <charles_lewis@icloud.com>
2023-12-24 12:01:03 +1100

    chore: remove config

--- remix.config.js
+++ remix.config.js
@@ -2,8 +2,4 @@
 export default {
   ignoredRouteFiles: ['**/*/*.test.{js,jsx,ts,tsx}'],
   tailwind: true,
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
-  serverBuildPath: 'build/index.js',
 }
